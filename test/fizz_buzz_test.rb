7dfa2cef32065170488ced02c5b63ca8bf4cb125
Joe OBrien <joe@objo.com>
2016-12-21 12:39:22 -0500

    removing non-requested feature

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,11 +1,6 @@
 require 'test/unit'
 
 class FizzBuzz
-  def initialize(_start, _end)
-    raise "Start must be greater than zero" if _start < 1
-    raise "End must be 100 or less" if _end > 100
-  end
-
   def translate(number)
     if (number % 3 == 0) && (number % 5 == 0)
       return "FizzBuzz"
@@ -19,24 +14,12 @@ class FizzBuzz
   end
 end
 
+
+
 class FizzBuzzTestCase < Test::Unit::TestCase
 
-  def test_start_must_be_greater_than_zero
-    exception = assert_raises RuntimeError do
-      FizzBuzz.new(-1, 1)
-    end
-    assert_match /Start must be greater than zero/, exception.message
-  end
-
-  def test_end_must_be_100_or_less
-    exception = assert_raises RuntimeError do
-      FizzBuzz.new(1, 101)
-    end
-    assert_match /End must be 100 or less/, exception.message
-  end
-
   def test_number_divisible_by_three_returns_fizz
-    f = FizzBuzz.new(1,100)
+    f = FizzBuzz.new
 
     assert_equal "Fizz", f.translate(3)
     assert_equal "Fizz", f.translate(6)
@@ -44,14 +27,14 @@ class FizzBuzzTestCase < Test::Unit::TestCase
   end
 
   def test_number_divisible_by_five_returns_buzz
-    f = FizzBuzz.new(1,100)
+    f = FizzBuzz.new
 
     assert_equal "Buzz", f.translate(5)
     assert_equal "Buzz", f.translate(10)
   end
 
   def test_numbers_not_divisible_by_three_or_five_return_actual_number
-    f = FizzBuzz.new(1,100)
+    f = FizzBuzz.new
 
     assert_equal 1, f.translate(1)
     assert_equal 2, f.translate(2)
@@ -60,7 +43,7 @@ class FizzBuzzTestCase < Test::Unit::TestCase
   end
 
   def test_numbers_divisible_by_both_returns_fizzbuzz
-    f = FizzBuzz.new(1,100)
+    f = FizzBuzz.new
 
     assert_equal "FizzBuzz", f.translate(15)
     assert_equal "FizzBuzz", f.translate(30)
