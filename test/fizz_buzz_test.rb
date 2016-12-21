b0e325bceba3184515324075685a1837d26831e1
Joe OBrien <joe@objo.com>
2016-12-21 12:02:17 -0500

    End must be 100 or less

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -3,15 +3,21 @@ require 'test/unit'
 class FizzBuzz
   def initialize(_start, _end)
     raise "Start must be greater than zero" if _start < 1
+    raise "End must be 100 or less" if _end > 100
   end
 end
 
 class FizzBuzzTestCase < Test::Unit::TestCase
 
-  def test_no_numbers_less_than_one
+  def test_start_must_be_greater_than_zero
     assert_raises RuntimeError do
       FizzBuzz.new(-1, 1)
     end
   end
-  
+
+  def test_end_must_be_100_or_less
+    assert_raises RuntimeError do
+      FizzBuzz.new(1, 101)
+    end
+  end
 end
