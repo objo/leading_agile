f3c3f2d92f47ca587584f3ddaaa422c08e1012e5
Joe OBrien <joe@objo.com>
2016-12-21 12:40:43 -0500

    simplifying tests

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -18,34 +18,30 @@ end
 
 class FizzBuzzTestCase < Test::Unit::TestCase
 
+  def setup
+    @f = FizzBuzz.new
+  end
+  
   def test_number_divisible_by_three_returns_fizz
-    f = FizzBuzz.new
-
-    assert_equal "Fizz", f.translate(3)
-    assert_equal "Fizz", f.translate(6)
-    assert_equal "Fizz", f.translate(9)
+    assert_equal "Fizz", @f.translate(3)
+    assert_equal "Fizz", @f.translate(6)
+    assert_equal "Fizz", @f.translate(9)
   end
 
   def test_number_divisible_by_five_returns_buzz
-    f = FizzBuzz.new
-
-    assert_equal "Buzz", f.translate(5)
-    assert_equal "Buzz", f.translate(10)
+    assert_equal "Buzz", @f.translate(5)
+    assert_equal "Buzz", @f.translate(10)
   end
 
   def test_numbers_not_divisible_by_three_or_five_return_actual_number
-    f = FizzBuzz.new
-
-    assert_equal 1, f.translate(1)
-    assert_equal 2, f.translate(2)
-    assert_equal 4, f.translate(4)
-    assert_equal 7, f.translate(7)
+    assert_equal 1, @f.translate(1)
+    assert_equal 2, @f.translate(2)
+    assert_equal 4, @f.translate(4)
+    assert_equal 7, @f.translate(7)
   end
 
   def test_numbers_divisible_by_both_returns_fizzbuzz
-    f = FizzBuzz.new
-
-    assert_equal "FizzBuzz", f.translate(15)
-    assert_equal "FizzBuzz", f.translate(30)
+    assert_equal "FizzBuzz", @f.translate(15)
+    assert_equal "FizzBuzz", @f.translate(30)
   end
 end
